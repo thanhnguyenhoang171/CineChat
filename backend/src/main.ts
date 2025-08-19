5f790541a755c53cebddd30028429e02cf4ad330
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2025-08-19 14:41:18 +0700

    adding files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  console.log('Check port: ', configService.get('PORT'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
