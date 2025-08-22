e0586afe12e30348203d69937f492d756053240a
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2025-08-22 17:40:40 +0700

    init frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                '@': '/src',
+                '@styles': '/src/styles',
+                '@assets': '/src/assets',
+                '@components': '/src/components',
+                '@utils': '/src/utils',
+            },
+        },
+        server: {
+            port: parseInt(env.PORT),
+            proxy: {
+                '/api': {
+                    target: env.BACKEND_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
+});
