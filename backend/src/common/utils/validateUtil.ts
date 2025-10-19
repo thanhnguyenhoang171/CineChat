341612f79f3a8a521731e5d2a6dbf7ede78d8c20
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2025-10-20 01:32:05 +0700

    Enhance global validation pipe with custom error messages, refactor validation utilities for better module existence checks, and update roles service to validate permissions during role creation.

--- backend/src/common/utils/validateUtil.ts
+++ backend/src/common/utils/validateUtil.ts
@@ -2,55 +2,77 @@ import { BusinessCode } from '@common/constants/business-code';
 import { HttpStatusCode } from '@common/constants/http-status-code';
 import { ResponseMessage } from '@common/constants/response-message';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { boolean } from 'joi';
 import mongoose from 'mongoose';
 
-export const validateMongoId = (id: string) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    throw new HttpException(
-      {
-        code: BusinessCode.INVALID_MONGODB_ID,
-        errors: ResponseMessage[BusinessCode.INVALID_MONGODB_ID],
-      },
-      HttpStatusCode.BAD_REQUEST,
-    );
+export const validateMongoId = (
+  id:
+    | string
+    | string[]
+    | mongoose.Types.ObjectId
+    | mongoose.Types.ObjectId[]
+    | mongoose.Schema.Types.ObjectId
+    | mongoose.Schema.Types.ObjectId[],
+) => {
+  const ids = Array.isArray(id) ? id : [id]; // always auto convert to array
+  for (const _id of ids) {
+    const strId = _id.toString(); // convert ObjectId -> string
+    if (!mongoose.Types.ObjectId.isValid(strId)) {
+      throw new HttpException(
+        {
+          code: BusinessCode.INVALID_MONGODB_ID,
+          errors: ResponseMessage[BusinessCode.INVALID_MONGODB_ID],
+        },
+        HttpStatusCode.BAD_REQUEST,
+      );
+    }
   }
-};
-
-export const ensureUserExists = async (userModel: any, id: string) => {
-  const user = await userModel.findById(id);
-  if (!user) {
-    throw new HttpException(
-      {
-        code: BusinessCode.USER_NOT_FOUND,
-        errors: ResponseMessage[BusinessCode.USER_NOT_FOUND],
-      },
-      HttpStatusCode.NOT_FOUND,
-    );
-  }
-
-  return user;
 };
 
 export const ensureModuleExists = async (
   Module: any,
   key: string = '_id',
   filter: Record<string, any> | string,
-  code: string,
-  errors: string,
+  businessCode: string,
+  errorMessage: string[] | string,
   statusCode: number,
 ) => {
-  // nếu filter là string → tự động tạo object
   const condition = typeof filter === 'string' ? { [key]: filter } : filter;
-
-  const existedModule = await Module.findOne(condition);
-  if (!existedModule) {
+  const module = await Module.findOne(condition);
+  if (!module) {
     throw new HttpException(
       {
-        code: code,
-        errors: errors,
+        code: businessCode,
+        errors: errorMessage,
       },
       statusCode,
     );
   }
-  return existedModule;
+
+  return module;
+};
+
+export const ensureAllModuleExist = async (
+  Model: any,
+  ids: string[] | mongoose.Types.ObjectId[] | mongoose.Schema.Types.ObjectId[],
+  code: string,
+  statusCode: number,
+) => {
+  const idStrings = ids.map((id) => id.toString());
+  const count = await Model.countDocuments({ _id: { $in: idStrings } });
+  if (count !== ids.length) {
+    throw new HttpException({ code, errors: ResponseMessage[code] }, statusCode);
+  }
+};
+
+export const isModuleExist = async (
+  Module: any,
+  key: string = '_id',
+  filter: Record<string, any> | string,
+): Promise<boolean> => {
+  // nếu filter là string → tự động tạo object
+  const condition = typeof filter === 'string' ? { [key]: filter } : filter;
+
+  const existedModule = await Module.findOne(condition);
+  return !!existedModule; // return true if exist
 };
--- backend/src/modules/roles/roles.module.ts
+++ backend/src/modules/roles/roles.module.ts
@@ -3,10 +3,16 @@ import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Role, RoleSchema } from './schemas/role.schema';
+import { Permission, PermissionSchema } from '@modules/permissions/shemas/permission.schema';
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
-  imports: [MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Role.name, schema: RoleSchema },
+      { name: Permission.name, schema: PermissionSchema },
+    ]),
+  ],
 })
 export class RolesModule {}
