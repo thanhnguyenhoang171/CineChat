a32426bd7dd1c6b548ac475ccfcbd6a4b08f7fc1
Thane Nguyen <thanhnguyenhoang171official@gmail.com>
2025-11-23 22:44:23 +0700

    initial admin fe

--- frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes";
-
-export default function App() {
-  return <RouterProvider router={routes}/>;
-}
--- frontend/src/components/share/not.found.tsx
+++
@@ -1,4 +0,0 @@
-const NotFound = () => {
-  return <>Not found this page</>;
-};
-export default NotFound;
