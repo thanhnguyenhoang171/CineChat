06edc04640cbc542630f571d84ed421f5bbea62b
Thane Nguyen <thanhnguyenhoang171official@gmail.com>
2026-03-25 10:50:42 +0700

    chore(user-console): optimize next.config for monorepo

--- apps/user-console/next.config.ts
+++ apps/user-console/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ["@cinechat/types"], // Giúp Next.js hiểu và biên dịch code từ gói shared
 };
 
 export default nextConfig;
